799c723f18c735cef7fc8c22011cfdde12a65eb5
lharder <l.harder@gmx.de>
2020-01-15 21:23:36 +0100

    Removed unnecessary collectionfactory: replaced by simpler gameobject. Default listener upon Gamepad.create(), per Button custom listener possible. Bugfixed. Examples.

--- gamepad/Gamepad.lua
+++ gamepad/Gamepad.lua
@@ -2,18 +2,18 @@
 Gamepad = {}
 
 -- Create a Table with Gamepad functions
-function Gamepad.new( gamepadfactory, listener )
+function Gamepad.new( gamepad, listener )
 
 	local pad = {}
 	pad._et = {}
 
 	-- create gameobject for gamepad
-	pad.gameobject = collectionfactory.create( gamepadfactory ) 
-	msg.post( "/collection0/controls#gamepad", "register", {} )
+	pad.gameobject = gamepad
+	msg.post( pad.gameobject, "register", { listener = listener } )
 
 
-	function pad:addButton( name, x, y, texture, listener )
-		msg.post( "/collection0/controls#gamepad", "addButton", { name = name, x = x, y = y, texture = texture, listener = listener } )
+	function pad:addButton( name, x, y, listener, texture )
+		msg.post( pad.gameobject, "addButton", { name = name, x = x, y = y, texture = texture, listener = listener } )
 	end
 
 	return pad
